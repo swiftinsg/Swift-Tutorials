4af59373f32b0cfe72f35c3f31735959861fdf29
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-06-18 05:26:52 +0800

    Fixed web (i think)

---
+++ Web/next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/:path(documentation|tutorials)/:subpath*",
+        destination: "/index.html",
+      },
+      {
+        source: "/:path(css|js|data|images|downloads|favicon\.ico|favicon\.svg|img|theme-settings\.json|videos)/:subpath*",
+        destination: "/:path",
+      },
+    ]
+  },
+};
