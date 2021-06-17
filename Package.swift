0243fac1662a4250e79b9124190ada421e12c88e
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-06-17 20:09:33 +0800

    DocC setup

--- Package.swift
+++ Package.swift
@@ -20,9 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "swiftinsg",
-            dependencies: []),
-        .testTarget(
-            name: "swiftinsgTests",
-            dependencies: ["swiftinsg"]),
+            dependencies: [])
     ]
 )
