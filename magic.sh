c651bffda34dcdd2ddd6fb3c85f4318c16dea24e
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-06-18 19:15:59 +0800

    created magic.sh, added sample projects

---
+++ magic.sh
@@ -0,0 +1,24 @@
+read -p "Commit message: "  message
+
+# Build Project
+sh build.sh
+
+# Add everything to git
+git add .
+git status
+
+# Commit change
+read -p "Commit? (y/n) " commit
+
+if [ "$commit" == 'y' ]; then
+git commit -m "$message"
+
+# Push to main
+git push origin
+
+# Push to mirror
+git push mirror
+
+else
+echo "No commits made 😢"
+fi
