081b597af21f47f53d74bf7517eeb6a18bc3cf3c
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-06-18 18:32:24 +0800

    Finished Custom Buttons code

---
+++ Sources/swiftinsg/Documentation.docc/Resources/Quiz App/Buttons/Section 2/quiz-ch1-button-s2-1-ContentView.swift
@@ -0,0 +1,102 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    var questions = [Question(title: "What day is it?",
+                              option1: "Monday",
+                              option2: "Friday",
+                              option3: "Wednesday",
+                              option4: "Saturday",
+                              correctOption: 4),
+                     Question(title: "What framework are we using?",
+                              option1: "UIKit",
+                              option2: "SwiftUI",
+                              option3: "React Native",
+                              option4: "Flutter",
+                              correctOption: 2),
+                     Question(title: "Which company created Swift?",
+                              option1: "Apple",
+                              option2: "Orange",
+                              option3: "Google",
+                              option4: "Tinkercademy",
+                              correctOption: 1)]
+    
+    @State private var currentQuestion = 0
+    
+    @State private var isAlertPresented = false
+    @State private var isCorrect = false
+    
+    @State private var correctAnswers = 0
+    @State private var isModalPresented = false
+    
+    var body: some View {
+        VStack {
+            Text(questions[currentQuestion].title)
+                .padding()
+            
+            HStack {
+                VStack {
+                    Button {
+                        didTapOption(optionNumber: 1)
+                    } label: {
+                        Image(systemName: "triangle.fill")
+                        Text(questions[currentQuestion].option1)
+                    }
+                    Button(questions[currentQuestion].option2) {
+                        didTapOption(optionNumber: 2)
+                    }
+                }
+                .padding()
+                
+                VStack {
+                    Button(questions[currentQuestion].option3) {
+                        didTapOption(optionNumber: 3)
+                    }
+                    Button(questions[currentQuestion].option4) {
+                        didTapOption(optionNumber: 4)
+                    }
+                }
+                .padding()
+            }
+            .padding()
+        }
+        .alert(isPresented: $isAlertPresented) {
+            Alert(title: Text(isCorrect ? "Correct" : "Wrong"),
+                  message: Text(isCorrect ? "Congrats, you are kinda smart." : "How can you be getting this wrong?!"),
+                  dismissButton: .default(Text("OK")) {
+                currentQuestion += 1
+                
+                if currentQuestion == questions.count {
+                    isModalPresented = true
+                    currentQuestion = 0
+                }
+            })
+        }
+        .sheet(isPresented: $isModalPresented,
+               onDismiss: {
+            correctAnswers = 0
+        },
+               content: {
+            ScoreView(score: correctAnswers,
+                      totalQuestions: questions.count)
+        })
+    }
+    
+    func didTapOption(optionNumber: Int) {
+        if optionNumber == questions[currentQuestion].correctOption {
+            print("Correct!")
+            isCorrect = true
+            correctAnswers += 1
+        } else {
+            print("Wrong!")
+            isCorrect = false
+        }
+        isAlertPresented = true
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
