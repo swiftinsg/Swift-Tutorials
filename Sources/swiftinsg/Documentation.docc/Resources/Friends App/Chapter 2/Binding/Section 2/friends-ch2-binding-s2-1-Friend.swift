4800ca4e55579bd11d45707dd01af3b27539e286
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-07-02 11:44:50 +0800

    Finished section 2 of binding

---
+++ Sources/swiftinsg/Documentation.docc/Resources/Friends App/Chapter 2/Binding/Section 2/friends-ch2-binding-s2-1-Friend.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct Friend: Identifiable {
+    var id = UUID()
+    
+    var name: String
+    var icon: String
+    var school: String
+    
+    var slothImage: String
+    
+    var attack: Double
+    var defence: Double
+}
---
+++ Sources/swiftinsg/Documentation.docc/Resources/Friends App/Chapter 2/Binding/Section 2/friends-ch2-binding-s2-2-ContentView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var friends = [Friend(name: "YJ",
+                                 icon: "zzz",
+                                 school: "Tinkercademy",
+                                 slothImage: "sloth3"),
+                          Friend(name: "Jia Chen",
+                                 icon: "swift",
+                                 school: "Ngee Ann Poly",
+                                 slothImage: "sloth2"),
+                          Friend(name: "Zerui",
+                                 icon: "wifi",
+                                 school: "NUS High",
+                                 slothImage: "sloth1")]
+    
+    var body: some View {
+        NavigationView {
+            List($friends) { friend in
+                NavigationLink(destination: FriendDetailView(friend: friend)) { 
+                    Image(systemName: friend.wrappedValue.icon)
+                    
+                    VStack(alignment: .leading) {
+                        Text(friend.wrappedValue.name)
+                            .bold()
+                        Text(friend.wrappedValue.school)
+                    }
+                }
+            }
+            .navigationTitle("Friends")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
