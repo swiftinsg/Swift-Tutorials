f25dd6657df06d16f7290f93d100196b8026435d
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-07-07 20:20:54 +0800

    Created friends manipulation stuff

---
+++ Sources/swiftinsg/Documentation.docc/Resources/Friends App/Chapter 3/Section 1/friends-ch3-manipulation-s1-2-ContentView.swift
@@ -0,0 +1,57 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var friends = [Friend(name: "YJ",
+                                 icon: "zzz",
+                                 school: "Tinkercademy",
+                                 slothImage: "sloth3",
+                                 attack: 10,
+                                 defence: 5,
+                                 types: [.ice, .fire]),
+                          Friend(name: "Jia Chen",
+                                 icon: "swift",
+                                 school: "Ngee Ann Poly",
+                                 slothImage: "sloth2",
+                                 attack: 15,
+                                 defence: 15,
+                                 types: [.electric, .ice]),
+                          Friend(name: "Zerui",
+                                 icon: "wifi",
+                                 school: "NUS High",
+                                 slothImage: "sloth1",
+                                 attack: 6,
+                                 defence: 3,
+                                 types: [.normal, .grass, .fire])]
+    
+    var body: some View {
+        NavigationView {
+            List(0..<friends.count) { index in
+                NavigationLink(destination: FriendDetailView(friend: $friends[index])) {
+                    Image(systemName: friends[index].icon)
+                    
+                    VStack(alignment: .leading) {
+                        Text(friends[index].name)
+                            .bold()
+                        HStack {
+                            Text(friends[index].school)
+                            
+                            Spacer()
+                            
+                            ForEach(friends[index].types, id: \.rawValue) { type in
+                                Image(systemName: type.getSymbolName())
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Friends")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
