4ae0465273e2d58153a5d17e2c601f0d86be3254
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-07-13 17:42:09 +0800

    Update todos with sample completed todo

---
+++ Sources/swiftinsg/Documentation.docc/Resources/Todo/Chapter 1/Strikethrough/Section 1/todo-ch1-strikethrough-s1-1-ContentView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var todos = [Todo(title: "Feed the cat", isCompleted: true),
+                        Todo(title: "Play with cat"),
+                        Todo(title: "Get allergies"),
+                        Todo(title: "Run away from cat"),
+                        Todo(title: "Get a new cat")]
+    
+    var body: some View {
+        NavigationView {
+            List(todos) { todo in
+                Text(todo.title)
+            }
+            .navigationTitle("Todos")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
