bd118f83721e66b84724792fb13b0e603dc2c2a2
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-07-14 01:08:46 +0800

    Finished strikethrough tutorial content

---
+++ Sources/swiftinsg/Documentation.docc/Resources/Todo/Chapter 1/Strikethrough/Section 1/todo-ch1-strikethrough-s1-2-ContentView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var todos = [Todo(title: "Feed the cat", isCompleted: true),
+                        Todo(title: "Play with cat"),
+                        Todo(title: "Get allergies"),
+                        Todo(title: "Run away from cat"),
+                        Todo(title: "Get a new cat")]
+    
+    var body: some View {
+        NavigationView {
+            List(todos) { todo in
+                HStack {
+                    Text(todo.title)
+                }
+            }
+            .navigationTitle("Todos")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
