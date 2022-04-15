7aba547822544afe747425f14a5197692c00bf90
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-04-15 16:43:36 +0800

    unit 1 setup

---
+++ Sources/swiftinsg/Documentation.docc/2022/Unit 1/Resources/u1-1-text-2.swift
@@ -0,0 +1,12 @@
+import PlaygroundSupport
+import SwiftUI
+
+struct MyView: View {
+    
+    var body: some View {
+        Text("YJ Soon")
+    }
+}
+
+
+PlaygroundPage.current.setLiveView(MyView())
